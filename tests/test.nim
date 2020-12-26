71331e0af73057c004ebac6f610a70b871965eec
Mostik <kirill.mostimenko@gmail.com>
2020-12-26 23:36:29 +0300

    chore: vertion -> version

--- tests/test.nim
+++ tests/test.nim
@@ -6,8 +6,8 @@ oparser(parser):
     echo "Welcome!"
   parser.add("-s", "--say", "Say my phrase", myphrase):
     echo "Say: ", myphrase
-  parser.add("-v", "--vertion", "Application version"):
-    echo "Vertion 0.0.1"
+  parser.add("-v", "--version", "Application version"):
+    echo "Version 0.0.1"
   parser.add("-h", "--help", "Show this help"):
     parser.help
     quit()
